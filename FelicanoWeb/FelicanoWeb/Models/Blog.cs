d0755ee2fab8bc1ea4bdbc18bcd4ffcd00242f8d
ngocphuocpxu <70925960+ngocphuocpxu@users.noreply.github.com>
2020-11-10 08:20:39 +0700

    res

---
+++ FelicanoWeb/FelicanoWeb/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FelicanoWeb.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortContent { get; set; }
+        public string Content { get; set; }
+        public string PublishDate { get; set; }
+        public string Views { get; set; }
+        public string Likes { get; set; }
+
+    }
+}
